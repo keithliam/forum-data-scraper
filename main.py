fb7453d2362d36c6dd3a156db211352feb43bbd3
keithliam <keithliamm@gmail.com>
2018-06-07 11:50:53 +0800

    Add html parser

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 from bs4 import BeautifulSoup
 import urllib.request
+
+url = 'https://stackoverflow.com/questions/927358/how-to-undo-the-most-recent-commits-in-git'
+
+html = urllib.request.urlopen(url)
+index = html.read().decode('utf-8')
+soup = BeautifulSoup(index, "html.parser")
