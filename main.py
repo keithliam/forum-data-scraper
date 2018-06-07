7270e1540101202de7c8d5ca8bd51af774e7cec8
keithliam <keithliamm@gmail.com>
2018-06-07 13:57:45 +0800

    Add question header extractor

--- main.py
+++ main.py
@@ -6,3 +6,9 @@ url = 'https://stackoverflow.com/questions/927358/how-to-undo-the-most-recent-co
 html = urllib.request.urlopen(url)
 index = html.read().decode('utf-8')
 soup = BeautifulSoup(index, "html.parser")
+
+data = {}
+data['question'] = {}
+data['answer'] = []
+
+data['question']['header'] = soup.find(id="question-header").h1.string
