2e596f7d42a2f3980f903bec545765ae0cb43117
keithliam <keithliamm@gmail.com>
2018-06-08 13:36:59 +0800

    Add output json fucntionality

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from StackoverflowParser import StackoverflowParser
+from FileOutput import outputJSONFile
 
 url = ''
 while url == '':
 	url = input('Enter stackoverflow page URL: ').strip()
 
 parser = StackoverflowParser(url)
-print(parser.parse())
+outputJSONFile(parser.parse())
